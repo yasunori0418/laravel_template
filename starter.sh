b61252a6829d5b8405dd77765ebced2daaf1db03
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-02-28 00:24:45 +0900

    Update: starter.sh

--- starter.sh
+++ starter.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+[[ ! -d ./src ]] && mkdir ./src
 docker compose up -d
-docker compose exec app composer install
+sleep 3
+docker compose exec app composer --prefer-dist create-project "laravel/laravel=9.*" .
 docker compose exec app chmod -R 775 bootstrap/cache
 docker compose exec app chmod -R 777 storage/
