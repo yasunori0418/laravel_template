9390b4b33bf5e184080f1a14aee66c8fe1726aae
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-02-27 23:45:48 +0900

    fix: typo

--- starter.sh
+++ starter.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 docker compose up -d
-docker comopse exec app composer install
-docker comopse exec app chmod -R 775 bootstrap/cache
-docker comopse exec app chmod -R 777 storage/
+docker compose exec app composer install
+docker compose exec app chmod -R 775 bootstrap/cache
+docker compose exec app chmod -R 777 storage/
