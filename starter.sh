92683412c0d5ee22b3a375983136a5354bfc50ec
yasunori-kirin0418 <yasunori.kirin0418@gmail.com>
2023-03-01 22:40:21 +0900

    docs: スクリプトにコメントを追加

--- starter.sh
+++ starter.sh
@@ -2,7 +2,11 @@
 
 [[ ! -d ./src ]] && mkdir ./src
 docker compose up -d
+
+# docker compose up -dの直後にlaravelプロジェクトを作成するとエラーになるので、少しだけ止める。
 sleep 3
+
+# Laravelプロジェクトの新規作成
 docker compose exec app composer --prefer-dist create-project "laravel/laravel=9.*" .
 docker compose exec app chmod -R 775 bootstrap/cache
 docker compose exec app chmod -R 777 storage/
